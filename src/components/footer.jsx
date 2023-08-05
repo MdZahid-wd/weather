7143ac6afcc1e431f27024f190c65db18e8db333
MdZahid-wb <zahidmd9830@gmail.com>
2023-08-05 06:08:31 +0530

    first commit

---
+++ src/components/footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../components/footer.css";
+function footer() {
+  return (
+    <div>
+      <footer className="footer">
+        <a className="code" href="https://github.com/MdZahid-wd" target="none">
+          github code
+        </a>
+      </footer>
+    </div>
+  );
+}
+
+export default footer;
